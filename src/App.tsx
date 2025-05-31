fa28d922dd34e3b1106cae692d4aabcebd10f8ae
huangyafeng <p-huangyafeng@xiaomi.com>
2025-05-31 14:48:45 +0800

    更新 package.json 和 yarn.lock 文件，添加 ahooks 依赖，提升项目功能。同时在 App.tsx 文件中注释掉了底部导航相关代码，保持代码整洁。

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View, Text, SafeAreaView } from 'react-native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+// import TaskHome from "./pages/Home"
 import styles from './app.style';
+// const Tab = createBottomTabNavigator()
 
 const App: React.FC = () => {
   return (
@@ -8,6 +11,13 @@ const App: React.FC = () => {
       <View style={styles.box}>
         <Text>Hello World</Text>
       </View>
+      {/* <Tab.Navigator>
+      <Tab.Screen
+            name="TaskHome"
+            component={TaskHome}
+            options={{ headerShown: false, title: '首页' }}
+          />
+      </Tab.Navigator> */}
     </SafeAreaView>
   );
 };
